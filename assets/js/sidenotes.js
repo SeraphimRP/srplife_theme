ff83da087173f062c2c03012f9c06a7cb805b80b
Seraphim R. Pardee <me@srp.life>
2025-06-24 02:04:44 -0400

    Overhaul initial commit.

--- assets/js/sidenotes.js
+++ assets/js/sidenotes.js
@@ -1,193 +1,210 @@
-let postClass = 'main-content-area';
-let postSelector = 'div.post-content';
-let notesWrapperSelector = '.notes-wrapper';
-let notesWrapperClass = 'notes-wrapper';
-let footnoteRefClass = 'footnote-ref';
-let footnoteRefSelector = '.footnote-ref';
+let postClass = "main-content-area";
+let postSelector = "div.post-content";
+let notesWrapperSelector = ".notes-wrapper";
+let footnoteRefSelector = ".footnote-ref";
 
 // Returns a function, that, as long as it continues to be invoked, will not
 // be triggered. The function will be called after it stops being called for
 // N milliseconds. If `immediate` is passed, trigger the function on the
 // leading edge, instead of the trailing.
 function debounce(func, wait, immediate) {
-  var timeout;
-  return function() {
-    var context = this, args = arguments;
-    var later = function() {
-      timeout = null;
-      if (!immediate) func.apply(context, args);
+    var timeout;
+
+    return function () {
+        var context = this,
+            args = arguments;
+
+        var later = function () {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+
+        var callNow = immediate && !timeout;
+
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+
+        if (callNow) func.apply(context, args);
     };
-    var callNow = immediate && !timeout;
-    clearTimeout(timeout);
-    timeout = setTimeout(later, wait);
-    if (callNow) func.apply(context, args);
-  };
-};
+}
 
 function showSidenotes() {
-  document.querySelector(postSelector).classList.remove("hide-sidenotes");
+    document.querySelector(postSelector).classList.remove("hide-sidenotes");
 }
 
 function hideSidenotes() {
-  document.querySelector(postSelector).classList.add("hide-sidenotes");
+    document.querySelector(postSelector).classList.add("hide-sidenotes");
 }
 
 function showEndnotes() {
-  document.querySelector(postSelector).classList.remove("hide-endnotes");
+    document.querySelector(postSelector).classList.remove("hide-endnotes");
 }
 
 function hideEndnotes() {
-  document.querySelector(postSelector).classList.add("hide-endnotes");
+    document.querySelector(postSelector).classList.add("hide-endnotes");
 }
 
-function insertSidenotes({showFootnotes}) {
-  const postContent = document.querySelector(postSelector);
-  for (const child of postContent.children) {
-    if (
-      child.classList.contains(notesWrapperClass) // notesWrapperSelector
-    ) {
-      continue;
-    }
-    const anchors = child.querySelectorAll(footnoteRefSelector); // footnoteRefClass
-    if (anchors.length) {
-      const sidenoteContainer = document.createElement("div");
-      sidenoteContainer.setAttribute("class", notesWrapperClass);
-      for (anchor of anchors) {
-        const aref = anchor.firstChild;
-        const id = aref.id;
-        const contentId = id.replace("ref", "");
-                //console.log(id, contentId);
-        const content = document.getElementById(contentId);
-        const sidenoteWrapper = document.createElement("aside");
-        sidenoteWrapper.setAttribute("id", id.replace("ref", ""));
-        sidenoteWrapper.setAttribute("class", "note");
-        sidenoteWrapper.setAttribute("role", "note");
-        sidenoteWrapper.setAttribute("data-anchor-id", id);
+function insertSidenotes() {
+    const postContent = document.querySelector(postSelector);
 
-        sidenoteWrapper.innerHTML = content.innerHTML;
-
-        // Remove "jump back to text" link, since it'll be right next to the anchor
-        const links = sidenoteWrapper.querySelectorAll("a");
-        const lastLink = links[links.length - 1];
-        if (lastLink && (lastLink.textContent === "↩︎")) {
-          lastLink.remove();
+    for (const child of postContent.children) {
+        if (child.classList.contains(notesWrapperSelector.slice(1))) {
+            continue;
         }
 
-        sidenoteWrapper.insertAdjacentHTML("afterbegin", `<div class='note-identifier'>${anchor.textContent}</div>`);
-        sidenoteContainer.insertAdjacentElement("beforeend", sidenoteWrapper);
-      }
-      child.insertAdjacentElement("afterend", sidenoteContainer);
+        const anchors = child.querySelectorAll(footnoteRefSelector);
+
+        if (anchors.length) {
+            const sidenoteContainer = document.createElement("div");
+            sidenoteContainer.setAttribute("class", notesWrapperSelector.slice(1));
+
+            for (let anchor of anchors) {
+                const aref = anchor.firstChild;
+                const id = aref.id;
+                const contentId = id.replace("ref", "");
+
+                const content = document.getElementById(contentId);
+                const sidenoteWrapper = document.createElement("aside");
+
+                sidenoteWrapper.setAttribute("id", id.replace("ref", ""));
+                sidenoteWrapper.setAttribute("class", "note");
+                sidenoteWrapper.setAttribute("role", "note");
+                sidenoteWrapper.setAttribute("data-anchor-id", id);
+
+                sidenoteWrapper.innerHTML = content.innerHTML;
+
+                // Remove "jump back to text" link, since it'll be right next to the anchor
+                const links = sidenoteWrapper.querySelectorAll("a");
+                const lastLink = links[links.length - 1];
+                if (lastLink && lastLink.textContent === "↩︎") {
+                    lastLink.remove();
+                }
+
+                sidenoteWrapper.insertAdjacentHTML("afterbegin", `<div class='note-identifier'>${anchor.textContent}</div>`);
+                sidenoteContainer.insertAdjacentElement("beforeend", sidenoteWrapper);
+            }
+
+            child.insertAdjacentElement("afterend", sidenoteContainer);
+        }
     }
-  }
 }
 
+// This function needs some work. Whatever it does, it offsets exponentially.
+// A solution would be to maybe force overlapping ones into a sort of table where
+// they can be effectively side-by-side while not looking like garbage.
 function positionSidenotes() {
-  const sidenotes = document.querySelectorAll("aside.note");
-  for (let i = 0; i < sidenotes.length; i++) {
-    const sidenote = sidenotes[i];
-    const anchorId = sidenote.getAttribute("data-anchor-id");
-    const anchor = document.querySelector(
-      `${postSelector} > *:not(${notesWrapperSelector}, .footnotes) #${anchorId}`
-    );
-    // const anchorParent = getAnchorParentContainer(anchor);
-    const anchorParent = anchor.parentNode;
+    const sidenotes = document.querySelectorAll("aside.note");
 
-    const anchorPosition = anchor.getBoundingClientRect().top;
-    const anchorParentPosition = anchorParent.getBoundingClientRect().top;
+    for (let i = 0; i < sidenotes.length; i++) {
+        const sidenote = sidenotes[i];
 
-    // Bump down sidenote if it would overlap with the previous one
-    let newPosition = anchorPosition;
-    if (i > 0) {
-      const prevSideNote = sidenotes[i - 1];
-      const prevSidenoteEnd = prevSideNote.getBoundingClientRect().bottom;
-      if (anchorPosition < prevSidenoteEnd) {
-        newPosition = prevSidenoteEnd + 20;
-      }
+        const anchorId = sidenote.getAttribute("data-anchor-id");
+        const anchor = document.querySelector(`${postSelector} > *:not(${notesWrapperSelector}, .footnotes) #${anchorId}`);
+        const anchorParent = anchor.parentNode;
+        const anchorPosition = anchor.getBoundingClientRect().top;
+        const anchorParentPosition = anchorParent.getBoundingClientRect().top;
+
+        // Bump down sidenote if it would overlap with the previous one
+        let newPosition = anchorPosition;
+        if (i > 0) {
+            const prevSideNote = sidenotes[i - 1];
+            const prevSidenoteEnd = prevSideNote.getBoundingClientRect().bottom;
+            if (anchorPosition < prevSidenoteEnd) {
+                newPosition = prevSidenoteEnd + 20;
+            }
+        }
+
+        let offset = Math.round(newPosition - anchorParentPosition);
+        if (offset != 0) {
+            sidenote.style.top = `${offset}px`;
+        }
+
+        // let offset = Math.round(newPosition - anchorParentPosition);
+        // if (offset != 0) {
+        //     sidenote.style.top = `calc(${offset}px - calc(${i + 1} * 3.25vh))`;
+        // } else if (i > 0) {
+        //     sidenote.style.top = `calc(-15vh - calc(${i + 1} * 6.5vh))`
+        // }
     }
-
-    let offset = Math.round(newPosition - anchorParentPosition);
-    if (offset != 0) {
-        sidenote.style.top = `${offset}px`;
-    }
-
-    // let offset = Math.round(newPosition - anchorParentPosition);
-    // if (offset != 0) {
-    //     sidenote.style.top = `calc(${offset}px - calc(${i + 1} * 3.25vh))`;
-    // } else if (i > 0) {
-    //     sidenote.style.top = `calc(-15vh - calc(${i + 1} * 6.5vh))`
-    // }
-  }
 }
 
-function insertAndPositionSidenotes({showFootnotes}) {
-  const mediaQuery = window.matchMedia("(min-width: 65rem)");
-  if (mediaQuery.matches) {
-    insertSidenotes({showFootnotes});
-    positionSidenotes();
-    hideEndnotes();
-    setTimeout(() => positionSidenotes(), 200);
-  }
+function insertAndPositionSidenotes({ showFootnotes }) {
+    const mediaQuery = window.matchMedia("(min-width: 65rem)");
+
+    if (mediaQuery.matches) {
+        insertSidenotes({ showFootnotes });
+        positionSidenotes();
+        hideEndnotes();
+        setTimeout(() => positionSidenotes(), 200);
+    }
 }
 
 function onResize() {
-  const sidenotesInDom = Boolean(document.querySelector(notesWrapperSelector));
-  //console.log('sidenotesInDom', sidenotesInDom);
-  const mediaQuery = window.matchMedia("(min-width: 65rem)");
-  if (mediaQuery.matches) {
-    if (!sidenotesInDom) {
-      insertSidenotes({showFootnotes: true});
+    const sidenotesInDom = Boolean(document.querySelector(notesWrapperSelector));
+    const mediaQuery = window.matchMedia("(min-width: 65rem)");
+
+    if (mediaQuery.matches) {
+        if (!sidenotesInDom) {
+            insertSidenotes({ showFootnotes: true });
+        }
+
+        showSidenotes();
+        hideEndnotes();
+        positionSidenotes();
+    } else {
+        if (sidenotesInDom) {
+            hideSidenotes();
+            showEndnotes();
+        }
     }
-    showSidenotes();
-    hideEndnotes();
-    positionSidenotes();
-  } else {
-    if (sidenotesInDom) {
-      hideSidenotes();
-      showEndnotes();
-    }
-  }
 }
 
 function onAnchorClick(evt) {
-  const mediaQuery = window.matchMedia("(min-width: 65rem)");
-  dehilightNotes();
-  if (mediaQuery.matches) {
-    const sidenote = document.querySelector("aside#" + evt.target.parentNode.firstChild.id.replace("ref", ""));
-    if (sidenote) {
-      evt.preventDefault();
-      evt.stopPropagation();
-    //   evt.target.classList.add("active-sidenote");
-      const sidenote = document.querySelector("aside#" + evt.target.parentNode.firstChild.id.replace("ref", ""));
-      sidenote.classList.add("active-sidenote");
-    }
-  }
-}
+    const mediaQuery = window.matchMedia("(min-width: 65rem)");
 
-function dehilightNotes(evt) {
-  const highlighted = document.querySelectorAll(".active-sidenote");
-  for (let highlight of highlighted) {
-    highlight.classList.remove("active-sidenote");
-  }
-}
+    dehilightNotes();
 
-function sidenotes({showFootnotes = true} = {}) {
-  if (document.getElementsByTagName('main')[0].classList.contains(postClass)) {
-    if (showFootnotes) {
-      window.addEventListener("resize", debounce(onResize, 100));
-      window.addEventListener("resize", debounce(onResize, 100));
-      const anchors = document.querySelectorAll(footnoteRefSelector);
-      for (const anchor of anchors) {
-        anchor.addEventListener("click", onAnchorClick);
-      }
-      document.addEventListener("click", (evt) => {
-        if (evt.target.nodeName !== "A") {
-          dehilightNotes();
+    if (mediaQuery.matches) {
+        const sidenote = document.querySelector("aside#" + evt.target.parentNode.firstChild.id.replace("ref", ""));
+
+        if (sidenote) {
+            evt.preventDefault();
+            evt.stopPropagation();
+
+            const sidenote = document.querySelector("aside#" + evt.target.parentNode.firstChild.id.replace("ref", ""));
+            sidenote.classList.add("active-sidenote");
         }
-      });
-      insertAndPositionSidenotes({showFootnotes});
     }
-
-  }
 }
 
-sidenotes({showFootnotes: true});
+function dehilightNotes() {
+    const highlighted = document.querySelectorAll(".active-sidenote");
+    for (let highlight of highlighted) {
+        highlight.classList.remove("active-sidenote");
+    }
+}
+
+function sidenotes({ showFootnotes = true } = {}) {
+    if (document.getElementsByTagName("main")[0].classList.contains(postClass)) {
+        if (showFootnotes) {
+            window.addEventListener("resize", debounce(onResize, 100));
+            window.addEventListener("resize", debounce(onResize, 100));
+
+            const anchors = document.querySelectorAll(footnoteRefSelector);
+
+            for (const anchor of anchors) {
+                anchor.addEventListener("click", onAnchorClick);
+            }
+
+            document.addEventListener("click", (evt) => {
+                if (evt.target.nodeName !== "A") {
+                    dehilightNotes();
+                }
+            });
+
+            insertAndPositionSidenotes({ showFootnotes });
+        }
+    }
+}
+
+sidenotes({ showFootnotes: true });
--- gulpfile.js
+++ gulpfile.js
@@ -1,103 +1,116 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass')(require('sass'));
-var postcss = require('gulp-postcss');
-var cleanCSS = require('gulp-clean-css');
-var autoprefixer = require('autoprefixer');
-var rename = require('gulp-rename');
-var wait = require('gulp-wait');
-var merge = require('merge2');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var del = require('del');
-var zip = require('gulp-zip');
-var browserSync = require('browser-sync').create();
+var gulp = require("gulp");
+var sass = require("gulp-sass")(require("sass"));
+var postcss = require("gulp-postcss");
+var cleanCSS = require("gulp-clean-css");
+var autoprefixer = require("autoprefixer");
+var rename = require("gulp-rename");
+var wait = require("gulp-wait");
+var merge = require("merge2");
+var concat = require("gulp-concat");
+var del = require("del");
+var zip = require("gulp-zip");
+var browserSync = require("browser-sync").create();
 
-gulp.task('css', function() {
-    var sassStream = gulp.src('./assets/scss/screen.scss')
-    .pipe(wait(100))
-    .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
-    .pipe(postcss([autoprefixer()]));
-    var cssStream = gulp.src([
-        './assets/css/bootstrap.min.css',
-        './assets/css/hl-styles/atom-one-dark.min.css'
-    ], { allowEmpty: true })
-    .pipe(concat('css-files.css'));
+gulp.task("css", function () {
+    var sassStream = gulp
+        .src("./assets/scss/screen.scss")
+        .pipe(wait(100))
+        .pipe(sass({ outputStyle: "expanded" }).on("error", sass.logError))
+        .pipe(postcss([autoprefixer()]));
+
+    var cssStream = gulp.src(["./assets/css/bootstrap.min.css", "./assets/css/katex.min.css", "./assets/css/prism-macchiato.css"], { allowEmpty: true }).pipe(concat("css-files.css"));
+
     return merge(cssStream, sassStream)
-    .pipe(concat('app.bundle.min.css'))
-    .pipe(cleanCSS({
-        level: {1: {specialComments: 0}},
-        compatibility: 'ie9'}))
-    .pipe(rename("bundle-css.hbs"))
-    .pipe(gulp.dest('./partials/styles'))
-    .pipe(browserSync.stream());
-});
-gulp.task('ampcss', function() {
-    return gulp.src(['./assets/scss/ampstyle-light.scss', './assets/scss/ampstyle-dark.scss'])
-    .pipe(wait(100))
-    .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
-    .pipe(postcss([autoprefixer()]))
-    .pipe(cleanCSS({
-        level: {1: {specialComments: 0}},
-        compatibility: 'ie9'}))
-    .pipe(rename({extname: ".hbs"}))
-    .pipe(gulp.dest('./partials/styles'));
-});
-gulp.task('concat-js', function() {
-    return gulp.src([
-        './assets/js/vendor/jquery-3.4.1.min.js',
-        './assets/js/vendor/jquery.fitvids.js',
-        './assets/js/vendor/highlight.pack.js',
-        './assets/js/vendor/searchinghost.min.js',
-        './assets/js/vendor/medium-zoom.min.js',
-        './assets/js/vendor/clipboard.min.js',
-        './assets/js/vendor/jquery.disqusloader.js',
-        './assets/js/index.js'
-    ], { allowEmpty: true })
-    .pipe(concat('app.bundle.min.js'))
-    // .pipe(uglify())
-    .pipe(gulp.dest('./assets/js'));
-});
-gulp.task('watch', gulp.series('css', 'ampcss', 'concat-js', function () {
-    browserSync.init({
-        proxy: "http://localhost:2368"
-    });
-    gulp.watch(['./assets/scss/**/*.scss', '!./assets/scss/ampstyle-light.scss', '!./assets/scss/ampstyle-dark.scss'], { allowEmpty: true }).on('change', gulp.series('css'));
-    gulp.watch(['./assets/scss/ampstyle-light.scss', './assets/scss/ampstyle-dark.scss']).on('change', gulp.series('ampcss'));
-    gulp.watch(['./assets/js/**/*.js', '!./assets/js/app.bundle.min.js'], { allowEmpty: true }).on('change', gulp.series('concat-js', browserSync.reload));
-    gulp.watch('./**/*.hbs').on('change', browserSync.reload);
-}));
-
-gulp.task('clean', function() {
-    return del(['./build', './dist']);
+        .pipe(concat("app.css"))
+        .pipe(
+            cleanCSS({
+                level: { 1: { specialComments: 0 } },
+                compatibility: "ie9",
+            })
+        )
+        .pipe(rename("app-css.hbs"))
+        .pipe(gulp.dest("build/partials/styles/"))
+        .pipe(browserSync.stream());
 });
 
-gulp.task('build', gulp.series('clean', 'css', 'concat-js', function () {
-    var targetDir = 'build/';
-
-    return gulp.src([
-        '**',
-        '!assets/scss', '!assets/scss/**/*',
-        '!assets/css', '!assets/css/**/*',
-        'assets/js/**', '!assets/js/vendor', '!assets/js/vendor/**/*',
-        '!node_modules', '!node_modules/**',
-        '!build', '!build/**',
-        '!dist', '!dist/**',
-        '!signup.hbs', '!signup-free.hbs',
-        '!signin.hbs', '!account.hbs'
-    ])
-    .pipe(gulp.dest(targetDir));
-}));
-
-gulp.task('zip', function () {
-    var targetDir = 'dist/';
-    var themeName = require('./package.json').name;
-    var filename = themeName + '.zip';
-
-    return gulp.src([
-        './build/**/*'
-    ])
-    .pipe(zip(filename))
-    .pipe(gulp.dest(targetDir));
+gulp.task("concat-js", function () {
+    return gulp
+        .src(
+            [
+                "./assets/js/vendor/jquery-3.7.1.min.js",
+                "./assets/js/vendor/jquery.fitvids.js",
+                "./assets/js/vendor/medium-zoom.min.js",
+                "./assets/js/vendor/clipboard.min.js",
+                "./assets/js/vendor/prism.min.js",
+                "./assets/js/vendor/prism-autoloader.min.js",
+                "./assets/js/vendor/searchinghost.min.js",
+                "./assets/js/index.js",
+            ],
+            { allowEmpty: true }
+        )
+        .pipe(concat("app.min.js"))
+        .pipe(gulp.dest("build/assets/js/"));
 });
 
-gulp.task('default', gulp.parallel('watch'));
+gulp.task(
+    "watch",
+    gulp.series("css", "concat-js", function () {
+        browserSync.init({
+            proxy: "http://localhost:2368",
+        });
+        gulp.watch(["./assets/scss/**/*.scss"], { allowEmpty: true }).on("change", gulp.series("css"));
+        gulp.watch(["./assets/js/**/*.js", "!./assets/js/app.min.js"], { allowEmpty: true }).on("change", gulp.series("concat-js", browserSync.reload));
+        gulp.watch("./**/*.hbs").on("change", browserSync.reload);
+    })
+);
+
+gulp.task("clean", function () {
+    return del(["./build", "./dist"]);
+});
+
+gulp.task(
+    "build",
+    gulp.series("clean", "css", "concat-js", function () {
+        return gulp
+            .src(
+                [
+                    "**",
+                    "assets/js/app.min.js",
+                    "assets/js/sidenotes.js",
+                    "!assets/js/index.js",
+                    "!assets/scss",
+                    "!assets/scss/**/*",
+                    "!assets/css",
+                    "!assets/css/**/*",
+                    "!assets/js/vendor/clipboard.min.js",
+                    "!assets/js/vendor/jquery-3.7.1.min.js",
+                    "!assets/js/vendor/jquery-migrate-3.5.0.min.js",
+                    "!assets/js/vendor/jquery.fitvids.js",
+                    "!assets/js/vendor/medium-zoom.min.js",
+                    "!assets/js/vendor/searchinghost.min.js",
+                    "!assets/js/vendor/prism-autoloader.min.js",
+                    "!assets/js/vendor/prism.min.js",
+                    "!node_modules",
+                    "!node_modules/**",
+                    "!build",
+                    "!build/**",
+                    "!dist",
+                    "!dist/**",
+                    "assets/js/vendor/katex.min.js",
+                    "assets/js/vendor/katex-auto-render.min.js",
+                ],
+                { allowEmpty: true }
+            )
+            .pipe(gulp.dest("build/"));
+    })
+);
+
+gulp.task("zip", function () {
+    var targetDir = "dist/";
+    var themeName = require("./package.json").name;
+    var filename = themeName + ".zip";
+
+    return gulp.src(["./build/**/*"]).pipe(zip(filename)).pipe(gulp.dest(targetDir));
+});
+
+gulp.task("default", gulp.parallel("watch"));
